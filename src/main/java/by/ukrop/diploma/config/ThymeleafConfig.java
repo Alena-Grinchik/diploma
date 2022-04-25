b19dc8f8c05b6508392f04a774663ae7c88424bd
Alena Grinchik <lena.grinchik@gmail.com>
2022-04-25 22:43:12 +0300

    v2 - template for header and footer added

---
+++ src/main/java/by/ukrop/diploma/config/ThymeleafConfig.java
@@ -0,0 +1,11 @@
+package by.ukrop.diploma.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+
+public class ThymeleafConfig {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
