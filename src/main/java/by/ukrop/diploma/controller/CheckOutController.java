1adf2afe199a17495a0c5fa36bb2881601433323
Alena Grinchik <lena.grinchik@gmail.com>
2022-04-28 22:47:01 +0300

    v2 - CheckOutForm updated

---
+++ src/main/java/by/ukrop/diploma/controller/CheckOutController.java
@@ -0,0 +1,31 @@
+package by.ukrop.diploma.controller;
+
+import by.ukrop.diploma.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+public class CheckOutController extends SuperController{
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/checkOut")
+    public String checkOut(HttpServletRequest request, HttpServletResponse httpResponse, Model model) throws IOException {
+        if (getCurrentOrderSize(request)==0){
+            httpResponse.sendRedirect("/menu");
+            return null;
+        }
+        HttpSession session = request.getSession(true);
+        Long currentCartId = (Long) session.getAttribute("CurrentCart");
+        model.addAttribute("currentOrder", orderService.getOrder(currentCartId));
+        return "checkOut";
+    }
+}
