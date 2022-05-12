0be5015bcf68fc6c68481be8f23c12de08bde8ef
Alena Grinchik <lena.grinchik@gmail.com>
2022-05-12 23:45:17 +0300

    edit dish

--- src/main/java/by/ukrop/diploma/controller/AddItemController.java
+++ src/main/java/by/ukrop/diploma/controller/AddItemController.java
@@ -1,8 +1,12 @@
 package by.ukrop.diploma.controller;
 
+import by.ukrop.diploma.persistence.entity.Dish;
+import by.ukrop.diploma.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,11 +15,19 @@ import java.io.IOException;
 @Controller
 public class AddItemController extends SuperController{
 
+    @Autowired
+    private DishService dishService;
+
     @GetMapping("/addItem")
-    public String addItem(HttpServletRequest request, HttpServletResponse httpResponse, Model model) throws IOException {
+    public String addItem(@RequestParam(required = false) Long dishId, HttpServletRequest request, HttpServletResponse httpResponse, Model model) throws IOException {
         if (model.getAttribute("currentUser") == null){
             httpResponse.sendRedirect("/signIn");
             return null;
+        }
+
+        if (dishId != null) {
+            Dish dish = dishService.getDish(dishId);
+            model.addAttribute("dish", dish);
         }
 
         return "addItem";
