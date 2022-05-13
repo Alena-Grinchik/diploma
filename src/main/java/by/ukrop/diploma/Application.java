c42a25309d1bb9897f1fca99e4ebdb9b30e90bc3
Alena Grinchik <lena.grinchik@gmail.com>
2022-05-13 21:00:17 +0300

    english language

--- src/main/java/by/ukrop/diploma/Application.java
+++ src/main/java/by/ukrop/diploma/Application.java
@@ -3,9 +3,38 @@ package by.ukrop.diploma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication(exclude= HibernateJpaAutoConfiguration.class)
-public class Application {
+public class Application implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+		interceptorRegistry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+
+		return localeChangeInterceptor;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		Locale defaultLocale = new Locale("ru");
+		slr.setDefaultLocale(defaultLocale);
+		return slr;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
