0b186843d1ed31d000ffb6d6a152a8a95c237d4f
Alena Grinchik <lena.grinchik@gmail.com>
2022-05-16 13:23:15 +0300

    index image gallery

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -16,5 +16,22 @@
                 form.classList.add('was-validated')
             }, false)
         })
+
+        let items = document.querySelectorAll('#photosCarousel .carousel-item')
+
+        items.forEach((el) => {
+            const minPerSlide = 3
+            let next = el.nextElementSibling
+            for (var i=1; i<minPerSlide; i++) {
+                if (!next) {
+                    // wrap carousel by using first child
+                    next = items[0]
+                }
+                let cloneChild = next.cloneNode(true)
+                el.appendChild(cloneChild.children[0])
+                next = next.nextElementSibling
+            }
+        })
+
     }, false)
 }())
